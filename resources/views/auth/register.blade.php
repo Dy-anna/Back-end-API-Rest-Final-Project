3bdd82192839168e5d53b25eadb1940dd490d830
maximilien <48603620+alpahroket@users.noreply.github.com>
2025-03-15 11:35:38 +0100

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.app')
-@section('title', 'Inscription')
+
 @section('content')
-    <div class="max-w-md mx-auto bg-white p-6 rounded shadow">
-        <h2 class="text-xl font-bold">Inscription</h2>
-        <form action="{{ route('register') }}" method="POST">
-            @csrf
-            <input type="text" name="name" placeholder="Nom" class="w-full p-2 border my-2">
-            <input type="email" name="email" placeholder="Email" class="w-full p-2 border my-2">
-            <input type="password" name="password" placeholder="Mot de passe" class="w-full p-2 border my-2">
-            <input type="password" name="password_confirmation" placeholder="Confirmer le mot de passe" class="w-full p-2 border my-2">
-            <button type="submit" class="w-full bg-green-500 text-white p-2">S'inscrire</button>
-        </form>
-    </div>
+<h1>Inscription</h1>
+<form method="POST" action="{{ route('register') }}">
+    @csrf
+    <input type="text" name="name" placeholder="Nom" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Mot de passe" required>
+    <input type="password" name="password_confirmation" placeholder="Confirmer mot de passe" required>
+    <button class="btn btn-primary">S'inscrire</button>
+</form>
 @endsection
