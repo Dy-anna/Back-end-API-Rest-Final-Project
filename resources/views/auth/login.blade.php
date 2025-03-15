663326915d5ed72c8216e3782ae5ff0166b2fb73
maximilien <48603620+alpahroket@users.noreply.github.com>
2025-03-15 11:35:20 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,11 @@
-@extends('layouts.app') 
-@section('title', 'Connexion')
+@extends('layouts.app')
+
 @section('content')
-    <div class="max-w-md mx-auto bg-white p-6 rounded shadow">
-        <h2 class="text-xl font-bold">Connexion</h2>
-        <form action="{{ route('login') }}" method="POST">
-            @csrf
-            <input type="email" name="email" placeholder="Email" class="w-full p-2 border my-2">
-            <input type="password" name="password" placeholder="Mot de passe" class="w-full p-2 border my-2">
-            <button type="submit" class="w-full bg-blue-500 text-white p-2">Se connecter</button>
-        </form>
-    </div>
+<h1>Connexion</h1>
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Mot de passe" required>
+    <button class="btn btn-primary">Se connecter</button>
+</form>
 @endsection
