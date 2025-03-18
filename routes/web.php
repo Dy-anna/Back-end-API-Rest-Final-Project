32e6407c35162133a951048ffa0ee0581dbb62fb
maximilien <48603620+alpahroket@users.noreply.github.com>
2025-03-18 11:08:27 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/events/search', [EventController::class, 'search'])->name('events.search');
 Route::get('/events/create', [EventController::class, 'create'])->name('events.create')->middleware('auth');
 Route::post('/events', [EventController::class, 'store'])->name('events.store')->middleware('auth');
+Route::get('/events/{id}', [EventController::class, 'show'])->name('events.show');
+Route::post('/events/{id}/register', [EventController::class, 'register'])->middleware('auth')->name('events.register');
+Route::post('/events/{id}/favorite', [EventController::class, 'favorite'])->middleware('auth')->name('events.favorite');
+Route::get('/favorites', [EventController::class, 'favorites'])->middleware('auth')->name('events.favorites');
+
 Route::middleware('auth')->group(function () {
     Route::get('/my-events', [EventController::class, 'myEvents'])->name('my-events'); // ✅ Voir les événements de l'utilisateur
     Route::get('/events/{id}/edit', [EventController::class, 'edit'])->name('events.edit');  // ✅ Formulaire d'édition
